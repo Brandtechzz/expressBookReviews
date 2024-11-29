7883577c1fff5e5aa4562b71e5b12cb1ae409740
Brandtechzz <brandonburke66@gmail.com>
2024-11-28 20:22:55 -0500

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -2,52 +2,58 @@ let books = {
     "978-0-14-028333-4": { // ISBN-13 format
         "author": "Chinua Achebe",
         "title": "Things Fall Apart",
-        "reviews": {}
+        "reviews": [
+            { "username": "user12", "review": "A compelling read showcasing cultural tensions." },
+            { "username": "user22", "review": "Insightful and thought-provoking." }
+        ]
     },
     "978-1-4028-4035-2": {
         "author": "Hans Christian Andersen",
-        "title": "Fairy tales",
-        "reviews": {}
+        "title": "Fairy Tales",
+        "reviews": [
+            { "username": "user12", "review": "A chilling glimpse into a dystopian future." },
+            { "username": "user13", "review": "Profoundly relevant even today." }
+        ]
     },
     "978-0-14-243722-3": {
         "author": "Dante Alighieri",
         "title": "The Divine Comedy",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     },
     "978-0-19-954037-1": {
         "author": "Unknown",
-        "title": "The Epic Of Gilgamesh",
-        "reviews": {}
+        "title": "The Epic of Gilgamesh",
+        "reviews": [] // Changed from {} to []
     },
     "978-0-19-51090-3": {
         "author": "Unknown",
-        "title": "The Book Of Job",
-        "reviews": {}
+        "title": "The Book of Job",
+        "reviews": [] // Changed from {} to []
     },
     "978-0-14-044913-6": {
         "author": "Unknown",
         "title": "One Thousand and One Nights",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     },
     "978-1-84511-290-0": {
         "author": "Unknown",
         "title": "Njál's Saga",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     },
     "978-0-19-953556-1": {
         "author": "Jane Austen",
         "title": "Pride and Prejudice",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     },
     "978-0-14-044202-1": {
         "author": "Honoré de Balzac",
         "title": "Le Père Goriot",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     },
     "978-0-85159-106-0": {
         "author": "Samuel Beckett",
         "title": "Molloy, Malone Dies, The Unnamable, the trilogy",
-        "reviews": {}
+        "reviews": [] // Changed from {} to []
     }
 };
 
